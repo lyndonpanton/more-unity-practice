17efd99f3eeaa8d466f46341161c97660f823dd8
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-03 03:26:25 +0000

    Completed problem 3

---
+++ Exercise13/Assets/Scripts/TeddyBear.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A teddy bear
+/// </summary>
+public class TeddyBear : MonoBehaviour
+{
+	/// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+	void Start()
+    {
+        // get the teddy bear moving
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.AddForce(
+            new Vector2(5, 0),
+            ForceMode2D.Impulse
+        );
+
+    }
+	
+}
